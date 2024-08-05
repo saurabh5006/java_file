21142e93aeba095fff4ba1d894a2e87db374c361
saurabh singh <saurabhsinghm5006@gmail.com>
2024-08-05 13:16:03 +0530

    change new1

---
+++ index.java
@@ -0,0 +1,8 @@
+package java_file;
+
+public class index{
+    public static void main(String[] args) {
+        System.out.println("saurabh singh");
+        System.out.println("singh ");
+    }
+}
